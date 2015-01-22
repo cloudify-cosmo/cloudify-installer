157a61868810f983cb08f6099a0437eb51281841
guy mograbi <guym@gigaspaces.com>
2015-01-22 10:43:33 +0200

    Update showVersionCommand.js

--- commands/showVersionCommand.js
+++ commands/showVersionCommand.js
@@ -1,6 +1,12 @@
 var actions = require('../lib/actions');
 var _ = require('lodash');
 
+/**
+ * @description lists all details on a version. 
+ * 
+ * @param cmd the cmd will be the version id to display
+ * 
+ * **/
 module.exports = function (cmd/*, options*/) {
     if(_.isString(cmd)) {
         actions.showVersion(cmd, function(err, version){
