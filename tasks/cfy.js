966b23db514d2d9829b7a4caf7a67da5adef8ffb
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-05-07 14:21:38 +0300

    fix(grunt): print error with stack trace.
    CFY-2712

--- tasks/cfy.js
+++ tasks/cfy.js
@@ -9,34 +9,38 @@ var logger = require('log4js').getLogger('grunt-cfy');
 //var cfyInstall = require('../lib/actions').install;
 
 module.exports = function (grunt) {
-    
+
     grunt.registerMultiTask('cfy', 'cloudify-installer', function () {
         var done = this.async();
         var data = this.data;
-
-        switch(data.cmd) {
-            case 'run_script' :
-                // args should have 'options' , 'cmd' and 'args'. see run_cmd
-                run_cmd( this.args , done );
-                break;
-            case 'list-available':
-                commands.listAvailableVersions().then(function(){
-                    done();
-                });
-                break;
-            case 'show-version':
-                logger.trace('executing show version');
-                actions.showVersion(data.version || '3.2', function(err, result){
-                    if(err) {
-                        console.log(result.error);
-                        done(false);
-                    }
-                    else {
-                        console.log(result.records);
-                        done(true);
-                    }
-                });
-                break;
+        try {
+            switch (data.cmd) {
+                case 'run_script' :
+                    // args should have 'options' , 'cmd' and 'args'. see run_cmd
+                    run_cmd(this.args, done);
+                    break;
+                case 'list-available':
+                    commands.listAvailableVersions().then(function () {
+                        done();
+                    });
+                    break;
+                case 'show-version':
+                    logger.trace('executing show version');
+                    actions.showVersion(data.version || '3.2', function (err, result) {
+                        if (err) {
+                            console.log(result.error);
+                            done(false);
+                        }
+                        else {
+                            console.log(result.records);
+                            done(true);
+                        }
+                    });
+                    break;
+            }
+        } catch (e) {
+            logger.error('cfy task failed', e);
+            throw e;
         }
     });
 
@@ -45,24 +49,24 @@ module.exports = function (grunt) {
         var options = this.options();
 
         // Chcek for prefix or use target name as default
-        if(!options.hasOwnProperty('prefix')) {
+        if (!options.hasOwnProperty('prefix')) {
             options.prefix = this.target;
         }
 
         // Check for version
-        if(!options.hasOwnProperty('version')) {
+        if (!options.hasOwnProperty('version')) {
             done(false, 'version is missing!');
         }
-        
+
         // Use virtualenv as default
-        if(!options.hasOwnProperty('env')) {
+        if (!options.hasOwnProperty('env')) {
             options.env = true;
         }
 
         // Install Cloudify
-        actions.install(options.version.toString(), options, function(err){
+        actions.install(options.version.toString(), options, function (err) {
             done(options.force ? true : err);
         });
     });
-    
+
 };
