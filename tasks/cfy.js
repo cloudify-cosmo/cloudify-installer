12590def3887f53f1d089681ce4c40dd5ad1b28e
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-05-07 14:14:19 +0300

    fix(grunt): handle scenario where args is empty for run_script
    CFY-2712

--- tasks/cfy.js
+++ tasks/cfy.js
@@ -16,7 +16,7 @@ module.exports = function (grunt) {
 
         switch(data.cmd) {
             case 'run_script' :
-                run_cmd( this.args, done );
+                run_cmd( this.args || [], done );
                 break;
             case 'list-available':
                 commands.listAvailableVersions().then(function(){
