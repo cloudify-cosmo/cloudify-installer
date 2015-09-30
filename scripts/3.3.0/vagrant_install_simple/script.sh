8f9b2066509acadc5a7868106821b3e9e4688b9f
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-09-30 16:09:18 +0300

    adding inputs file. I was mistaken to say they are not needed.

--- scripts/3.3.0/vagrant_install_simple/script.sh
+++ scripts/3.3.0/vagrant_install_simple/script.sh
@@ -92,7 +92,8 @@ if [ "$INSTALL_SYSTEM_TESTS_REQ" = "true" ]; then
     echo "NODECELLAR_BRANCH is $NODECELLAR_BRANCH"
 
     cfy blueprints publish-archive -l https://github.com/cloudify-cosmo/cloudify-nodecellar-example/archive/${NODECELLAR_BRANCH}.tar.gz -b nodecellar1 -n simple-blueprint.yaml
-    cfy deployments create -b nodecellar1 -d deployment_to_delete --inputs simple-inputs.yaml
-    cfy deployments create -b nodecellar1 -d installed_deployment --inputs simple-inputs.yaml
+    NODECELLAR_INPUTS_FILE=${DIR}/nodecellar_${USER}_inputs.yaml
+    cfy deployments create -b nodecellar1 -d deployment_to_delete --inputs ${NODECELLAR_INPUTS_FILE}
+    cfy deployments create -b nodecellar1 -d installed_deployment --inputs ${NODECELLAR_INPUTS_FILE}
     #cfy executions start -w install -d installed_deployment
 fi
