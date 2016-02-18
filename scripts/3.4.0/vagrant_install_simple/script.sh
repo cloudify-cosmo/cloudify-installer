b51a00a8a91a534c261e26b50c5918d5a898a024
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2016-02-18 17:22:29 +0200

    fixing refernce to inputs file

--- scripts/3.4.0/vagrant_install_simple/script.sh
+++ scripts/3.4.0/vagrant_install_simple/script.sh
@@ -110,8 +110,8 @@ fi
 
 if [ "${CLOUDIFY_INTALLER_INPUT_WEBUI_SOURCE_URL}" != "" ]; then
     echo "using alternative webui url ${CLOUDIFY_INSTALLER_INPUT_WEBUI_SOURCE_URL}"
-    echo "webui_source_url: \"${CLOUDIFY_INTALLER_INPUT_WEBUI_SOURCE_URL}\"" >> $INPUT_FILE
-    cat $INPUT_FILE
+    echo "webui_source_url: \"${CLOUDIFY_INTALLER_INPUT_WEBUI_SOURCE_URL}\"" >> $INPUTS_FILE
+    cat $INPUTS_FILE
 else
     echo "not overriding ui url"
 fi
